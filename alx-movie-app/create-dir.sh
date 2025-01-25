b98d30fc3d3de3202972d4e33967d633675f8c7b
Donald-Edinam <kobbyedinamdonald@gmail.com>
2025-01-25 23:14:27 +0000

    Modidied files

---
+++ alx-movie-app/create-dir.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Create directories and files
+mkdir -p components/commons components/layouts
+
+touch components/commons/Button.tsx
+touch components/commons/Loading.tsx
+touch components/commons/MovieCard.tsx
+
+touch components/layouts/Header.tsx
+touch components/layouts/Footer.tsx
+touch components/layouts/Layout.tsx
+touch components/layouts/Button.tsx
+
+echo "Directories and files created successfully."
